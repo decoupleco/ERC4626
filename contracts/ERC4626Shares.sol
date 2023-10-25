551eafb463fddf9051dac3bca0f5684bc16fa4ae
markgk629 <markgk629@gmail.com>
2023-10-25 15:03:44 +0800

    Initial commit

---
+++ contracts/ERC4626Shares.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import { ERC4626, ERC20 } from '@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol';
+
+contract ERC4626Shares is ERC4626 {
+  constructor(
+    ERC20 _underlying,
+    string memory _name,
+    string memory _symbol
+  ) ERC4626(_underlying) ERC20(_name, _symbol) {}
+}
