83258f5c71c36fa9b655575abc77094ef183b3b5
markgk629 <markgk629@gmail.com>
2023-10-28 14:49:15 +0800

    [spec] Update xERC4626 redeem specs

--- test/xERC4626.spec.js
+++ test/xERC4626.spec.js
@@ -94,11 +94,12 @@ describe('[CustomXERC4626]', () => {
     const amountIn = parseUnits('1', 18)
     const reward = parseUnits('0.1', 18)
     let shares
+
     before(async () => {
       shares = await contract.maxRedeem(user1.address)
     })
 
-    after(async () => {
+    afterEach(async () => {
       await utils.printBalances(
         { deployer, user1, contract },
         { token },
@@ -108,17 +109,19 @@ describe('[CustomXERC4626]', () => {
 
     it('3.1 add rewards to pool will not change totalAssets', async () => {
       await token.mint(contract.target, reward)
+
       expect(await contract.lastRewardAmount()).to.be.equal(0n)
       expect(await contract.totalAssets()).to.be.equal(amountIn)
       expect(await contract.convertToAssets(shares)).to.be.equal(amountIn)
+      expect(await contract.lastSync()).to.be.equal(0n)
       // contract token balance is 1.1, but totalAssets is still 1
       expect(await token.balanceOf(contract.target)).to.be.equal(
         amountIn + reward
       )
     })
 
-    it('3.2 after sync, everything same except lastRewardAmount', async () => {
-      expect(await contract.lastSync()).to.be.equal(0n)
+    it('3.2 after sync, lastSync, lastRewardAmount & rewardsCycleEnd will be updated', async () => {
+      const rewardsCycleEndBefore = await contract.rewardsCycleEnd()
 
       await contract.syncRewards()
 
@@ -126,9 +129,25 @@ describe('[CustomXERC4626]', () => {
       expect(await contract.totalAssets()).to.be.equal(amountIn)
       expect(await contract.convertToAssets(shares)).to.be.equal(amountIn)
       expect(await contract.lastSync()).to.be.gt(0n)
+      // contract token balance is 1.1, but totalAssets is still 1
+      expect(await token.balanceOf(contract.target)).to.be.equal(
+        amountIn + reward
+      )
+      const rewardsCycleEndAfter = await contract.rewardsCycleEnd()
+      expect(rewardsCycleEndAfter).to.be.gt(rewardsCycleEndBefore)
+      console.log({
+        rewardsCycleEndBefore: new Date(
+          +rewardsCycleEndBefore.toString() * 1000
+        ).toLocaleString(),
+        rewardsCycleEndAfter: new Date(
+          +rewardsCycleEndAfter.toString() * 1000
+        ).toLocaleString()
+      })
     })
 
     it('3.3 accrue part of the rewards', async () => {
+      const userAssetsBefore = await contract.convertToAssets(shares)
+
       const blockTimestamp = BigInt(await time.latest())
       const rewardsCycleEnd = await contract.rewardsCycleEnd()
       await time.setNextBlockTimestamp(
@@ -152,12 +171,93 @@ describe('[CustomXERC4626]', () => {
       expect(totalAssets).to.be.gt(amountIn)
       expect(totalAssets).to.be.lt(amountIn + reward)
 
-      const userAssets = await contract.convertToAssets(shares)
-      expect(userAssets).to.be.gt(amountIn)
-      expect(userAssets).to.be.lt(amountIn + reward)
+      const userAssetsAfter = await contract.convertToAssets(shares)
+      expect(userAssetsAfter).to.be.gt(amountIn)
+      expect(userAssetsAfter).to.be.lt(amountIn + reward)
       console.log({
-        userAssets: formatUnits(userAssets, 18)
+        userAssetsBefore: formatUnits(userAssetsBefore, 18),
+        userAssetsAfter: formatUnits(userAssetsAfter, 18)
       })
+    })
+
+    it('3.4 accrue all of the remaining rewards', async () => {
+      const userAssetsBefore = await contract.convertToAssets(shares)
+
+      const rewardsCycleEnd = await contract.rewardsCycleEnd()
+      await time.setNextBlockTimestamp(rewardsCycleEnd + 1n)
+      await mine(1)
+
+      console.log({
+        lastSync: new Date(
+          +(await contract.lastSync()).toString() * 1000
+        ).toLocaleString(),
+        blockTimestamp: new Date((await time.latest()) * 1000).toLocaleString(),
+        rewardsCycleEnd: new Date(
+          +(await contract.rewardsCycleEnd()).toString() * 1000
+        ).toLocaleString()
+      })
+
+      expect(await contract.lastRewardAmount()).to.be.equal(reward)
+
+      const totalAssets = await contract.totalAssets()
+      console.log({ totalAssets })
+      expect(totalAssets).to.be.gt(amountIn)
+      expect(totalAssets).to.be.eq(amountIn + reward)
+
+      const userAssetsAfter = await contract.convertToAssets(shares)
+      expect(userAssetsAfter).to.be.gt(userAssetsBefore)
+      expect(userAssetsAfter).to.be.eq(amountIn + reward)
+      console.log({
+        userAssetsBefore: formatUnits(userAssetsBefore, 18),
+        userAssetsAfter: formatUnits(userAssetsAfter, 18)
+      })
+    })
+  })
+
+  describe('4. [redeem]', () => {
+    afterEach(async () => {
+      await utils.printBalances(
+        { deployer, user1, contract },
+        { token },
+        { skipETH: true }
+      )
+    })
+
+    it('4.1 user can redeem half of the shares', async () => {
+      const totalShares = await contract.maxRedeem(user1.address)
+      const balanceBefore = await token.balanceOf(user1.address)
+
+      await contract
+        .connect(user1)
+        .redeem(totalShares / 2n, user1.address, user1.address)
+
+      const sharesAfter = await contract.maxRedeem(user1.address)
+      expect(sharesAfter).to.be.lt(totalShares)
+
+      const balanceAfter = await token.balanceOf(user1.address)
+      expect(balanceAfter).to.be.gt(balanceBefore)
+
+      await utils.printBalances(
+        { deployer, user1, contract },
+        { token },
+        { skipETH: true }
+      )
+    })
+
+    it('4.2 user can redeem all remaining shares', async () => {
+      await contract.syncRewards()
+
+      const sharesBefore = await contract.maxRedeem(user1.address)
+
+      await contract
+        .connect(user1)
+        .redeem(sharesBefore, user1.address, user1.address)
+
+      const sharesAfter = await contract.maxRedeem(user1.address)
+      expect(sharesAfter).to.be.equal(0n)
+
+      const balance = await token.balanceOf(user1.address)
+      expect(balance).to.be.equal(parseUnits('1.1', 18))
     })
   })
 })
